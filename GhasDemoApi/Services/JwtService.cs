b64f066c0863e72eeb86fb1c922e495840627ad1
v-yilmazseda <v-yilmazseda@microsoft.com>
2026-02-06 12:53:46 +0300

    denmo

---
+++ GhasDemoApi/Services/JwtService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace GhasDemoApi.Services
+{
+   
+
+    public class JwtService
+    {
+        // ❌ Secret scanning
+        private const string JwtKey = "sk_live_1234567890SUPERSECRET";
+
+        public SymmetricSecurityKey GetKey()
+            => new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(JwtKey));
+    }
+}
